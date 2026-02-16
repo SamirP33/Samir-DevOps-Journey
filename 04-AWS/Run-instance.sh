9978e4a2b9179d0717890dc7228eef2f25649b7b
Samir Patil <your-github-sameerpatil21351@gmail.com>
2026-02-16 18:40:24 +0000

    New script has been loaded

---
+++ 04-AWS/Run-instance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INSTANCE_ID="i-02b6ec1e0d5eb791f"
+
+Start_instance() {
+    echo "Starting EC2 Instance"
+    aws ec2 start-instances --instance-ids $INSTANCE_ID
+    echo "EC2 Instance has been started"
+}
+
+Stop_instance() {
+    echo "Stopping EC2 Instance"
+    aws ec2 stop-instances --instance-ids $INSTANCE_ID
+    echo "EC2 Instance has been stopped"
+}
+
+if [[ $1 == "start" ]]; then
+    Start_instance
+
+elif [[ $1 == "stop" ]]; then
+    Stop_instance
+
+else
+    echo "Usage: $0 {start|stop}"
+fi
+
